4d8292cd0f1de18d193f36187ea9e61f7a47328b
Edward Mendoza <lalo.garibo90@gmail.com>
2020-04-29 16:47:57 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[$@!#^*({]/gi, "");
+  }
+
+  static titleize(str) {
+    const PRHASES = [
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "and",
+      "from",
+      "the",
+    ];
+    let words = str.split(" ");
+    let finalWords = [];
+    words.forEach((word) => {
+      if (PRHASES.includes(word)) {
+        finalWords.push(word);
+      } else {
+        word = word.charAt(0).toUpperCase() + word.slice(1);
+        finalWords.push(word);
+      }
+    });
+    return finalWords.join(" ");
+  }
 }
